c4a642e03d77572e666eb1a5c55f882167de6970
kajal1301 <37709194+kajal1301@users.noreply.github.com>
2020-10-04 13:47:21 +0530

    Update Permulations.py

--- ITERTOOL/Permulations.py
+++ ITERTOOL/Permulations.py
@@ -1,9 +1,14 @@
 from itertools import permutations
 
-S="ARPIT"
+S=input("Enter string:")
+P= list(map(int,input("Enter list:").split()))
 k=5 #length of required Permutation
 #provide sorted string will get sorted permutation
 li=list(permutations(sorted(S),k))
+lj= list(permutations(sorted(P),k))
 print(len(li))
+print(len(lj))
 for i in li:
     print(''.join(i))
+for j in lj:
+    print(j)
