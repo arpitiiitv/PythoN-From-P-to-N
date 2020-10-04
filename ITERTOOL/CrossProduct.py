37322431c339362b60aeec43abe047f7fd84d236
kajal1301 <37709194+kajal1301@users.noreply.github.com>
2020-10-04 13:39:11 +0530

    Update CrossProduct.py

--- ITERTOOL/CrossProduct.py
+++ ITERTOOL/CrossProduct.py
@@ -1,6 +1,9 @@
 # finding cross product of 2 list
 from itertools import product
-A=[1,2,3,4,5]
-B=[6,7,8]
+#A=[1,2,3,4,5]
+A = list(map(int, input("Enter A").split()))
+B = list(map(int, input("Enter B").split()))
+#B=[6,7,8]
 cross_product=list(product(A,B))
 print(cross_product)
+                       
