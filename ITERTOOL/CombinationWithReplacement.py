f18c46a21c77e39bdb7d22b3c953afccd37dfd06
kajal1301 <37709194+kajal1301@users.noreply.github.com>
2020-10-04 13:44:19 +0530

    Update CombinationWithReplacement.py
    combination with replacement of string and list.

--- ITERTOOL/CombinationWithReplacement.py
+++ ITERTOOL/CombinationWithReplacement.py
@@ -1,8 +1,13 @@
 from itertools import combinations_with_replacement as cwr
 S="ARPIT"
-k=3 #LENGTH OF SUBSTRING OF s
+P= list(map(int, input("enter list").split()))
+k=int(input("Enter the length of substring")) #LENGTH OF SUBSTRING OF s
 Combination_result=list(cwr(S,k))
+combination_res= list(cwr(P,k))
 print(len(Combination_result))
-
+print(len(combination_res))
 for i in Combination_result:
     print(''.join(i),end=",")
+for i in combination_res:
+    print(i)
+    
